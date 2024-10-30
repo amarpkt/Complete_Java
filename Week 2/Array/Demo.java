9cc57caae2d9b73a5c7c9c0f36331cdaaf974182
Amar Pathak <amarpathak723@gmail.com>
2024-10-30 22:09:58 +0530

    301024 Added Arrays Folder and added first code for array

---
+++ Week 2/Array/Demo.java
@@ -0,0 +1,19 @@
+public class Demo {
+    public static void main(String[] args) {
+        int nums[] = {3, 7, 2, 4};
+
+        System.out.println(nums[1]);
+
+        nums[1] = 6;
+
+        System.out.println(nums[1]);
+
+        // Printing all values without sysout i.e. using loop
+        for(int i = 0 ; i < 4 ; i++){
+            System.out.print(nums[i] + " ");
+        }
+
+
+    }    
+
+}
