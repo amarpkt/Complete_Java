47d1a64a62a2715e51bf5389de5da0623f95d4ae
Amar Pathak <amarpathak723@gmail.com>
2024-11-06 23:25:15 +0530

    final commit for 06112024

---
+++ Week 2/String/Demo.java
@@ -0,0 +1,14 @@
+public class Demo {
+ public static void main(String[] args) {
+    System.out.println("begin");
+    String name = new String("amar");
+    System.out.println(name);
+
+    System.out.println(name.hashCode());
+
+    System.out.println("hello " + name);
+
+    System.out.println(name.concat(" pathak"));
+
+ }   
+}
