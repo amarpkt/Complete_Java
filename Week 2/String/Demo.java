d08d1253427917d757d221f9bdd317e01b674141
Amar Pathak <amarpathak723@gmail.com>
2024-11-07 13:38:13 +0530

    first commit for 07112024

--- Week 2/String/Demo.java
+++ Week 2/String/Demo.java
@@ -10,5 +10,11 @@ public class Demo {
 
     System.out.println(name.concat(" pathak"));
 
+    String s1 = "Naveen";
+    String s2 = "Naveen";
+    System.out.println(s1.equals(s2));
+
+    System.out.println(s1.hashCode() + " :  " + s2.hashCode() + " : " + name.hashCode() );
+
  }   
 }
---
+++ Week 2/String/Demo2.java
@@ -0,0 +1,13 @@
+public class Demo2 {
+ public static void main(String[] args) {
+    
+    String name = "naveen";
+    System.out.println(name);
+    System.out.println(name.hashCode());
+    name = name + " reddy";
+    System.out.println(name);
+    System.out.println(name.hashCode());
+
+
+ }   
+}
