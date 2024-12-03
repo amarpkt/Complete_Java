08a2b81f16c799dad9d4659e06144c0c6f6296ee
Amar Pathak <amarpathak723@gmail.com>
2024-12-03 09:24:37 +0530

    031224 Interface and interface need

--- Week 4/Interface/Demo.java
+++ Week 4/Interface/Demo.java
@@ -1,28 +1,5 @@
-public class Demo{
+public class Demo {
     public static void main(String[] args) {
         System.out.println("begin");
-
-        Parent obj1 = new ChildA();
-        Parent obj2 = new ChildB();
-
-        obj1.show();
-        obj2.show();
-
-    }
-}
-
-class ChildA extends Parent{
-    public void show(){
-        System.out.println("In Child A Show");
-    }
-}
-class ChildB extends Parent{
-    public void show(){
-        System.out.println("In Child B Show");
-    }
-}
-class Parent {
-    public void show(){
-        System.out.println("In Parent Show");
     }
 }
