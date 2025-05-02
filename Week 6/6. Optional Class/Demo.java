5566b8216bdd61254b85f8e21e0c81edb31e39d0
Amar Pathak <amarpathak723@gmail.com>
2025-05-02 10:12:43 +0530

    02052025 Optional Class

--- Week 6/6. Optional Class/Demo.java
+++ Week 6/6. Optional Class/Demo.java
@@ -1,5 +1,26 @@
+
+import java.util.Arrays;
+import java.util.List;
+
+
+
 public class Demo{
     public static void main(String[] args) {
-        System.out.println("Test");
+        // System.out.println("Test");
+        List<String> names = Arrays.asList("Naveen", "Jhon", "Lakshmi", "Kishor");
+
+        // System.out.println(names);
+
+        String name = names.stream()
+                        .filter(str -> str.contains("x"))
+                        .findFirst()
+                        .orElse("Not foundd");
+                        
+        System.out.println(name);
+        // System.out.println(name.orElse("not found"));
+        
+        
+
+
     }
 }
