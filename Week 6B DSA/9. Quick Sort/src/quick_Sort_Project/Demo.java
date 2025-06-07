48ecdc4d2ae95c357b6f3b091e689a6614500bb0
Amar Pathak <amarpathak723@gmail.com>
2025-06-07 17:58:36 +0530

    07062024 Added Quick Sort Theory and Code and Concept of Divide & Conquer, Tree

---
+++ Week 6B DSA/9. Quick Sort/src/quick_Sort_Project/Demo.java
@@ -0,0 +1,48 @@
+package quick_Sort_Project;
+
+public class Demo {
+	public static void main(String[] args) { 
+
+		int arr[] = {5,6,3,1,8,4};
+		
+		System.out.println("Before sorting: ");
+		for(int num : arr) {
+			System.out.print(num + " ");
+		}
+		System.out.println();
+		quickSort(arr, 0, arr.length -1);
+		System.out.println();
+		System.out.println("After sorting: ");
+		for(int num : arr) {
+			System.out.print(num + " ");
+		}
+	}
+
+	public static void quickSort(int arr[], int low, int high) {
+		for(int num : arr) {
+			System.out.print(num + " ");
+		}
+		System.out.println();
+		if( low<high) {
+			int pi = partition(arr, low, high);
+			quickSort(arr, low, pi-1);
+			quickSort(arr, pi+1, high);
+		}
+	}
+	public static int partition(int arr[], int low, int high) {
+		int pivot = arr[high];
+		int i = low-1;
+		for(int j = low; j < high ; j++) {
+			if(arr[j] < pivot) {
+				i++;
+				int temp = arr[i];
+				arr[i] = arr[j];
+				arr[j] = temp;
+			}
+		}
+		int temp = arr[i+1];
+		arr[i+1] = arr[high];
+		arr[high] = temp;
+		return i+1;
+	}
+}
